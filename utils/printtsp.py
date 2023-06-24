7fcdd443d2ce7437f26ac5d4a8266ff36e7c2522
eduardoadf21 <eduardoadf.luis@gmail.com>
2023-06-24 09:52:38 -0300

    utils, renaming

--- utils/printtsp.py
+++ utils/printtsp.py
@@ -1,39 +1,29 @@
+
 from sys import maxsize
 import time
 from itertools import permutations
-from graph import generate_graph
-V = 7
+from distance import generate_graph
+V = 8
  
 def travellingSalesmanProblem(graph, s):
-    # store all vertex apart from source vertex
     vertex = []
     for i in range(V):
         if i != s:
             vertex.append(i)
  
-    # store minimum weight Hamiltonian Cycle
     min_path = maxsize
-    next_permutation = permutations(vertex)
-    count_perm = 0
+    next_permutation=permutations(vertex)
     for i in next_permutation:
-        count_perm += 1
  
         current_pathweight = 0
         k = s
-        print(i)
-        # compute current path weight
         for j in i:
             current_pathweight += graph[k][j]
-            print(k,j)
             k = j
         current_pathweight += graph[k][s]
-        print(k,s)
-        print(current_pathweight)
  
-        # update minimum
         min_path = min(min_path, current_pathweight)
 
-    print(f'permutações = {count_perm}')
     return min_path
 
 start_time = time.time() 
